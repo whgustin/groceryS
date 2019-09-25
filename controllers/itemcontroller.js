0f8071e103b567a01b8f2fde13822eb0b1d91cd4
Mike Gustin <mikegustin@Mikes-MacBook.local>
2019-09-25 13:52:26 -0400

    update put method fixed

--- controllers/itemcontroller.js
+++ controllers/itemcontroller.js
@@ -44,47 +44,14 @@ router.delete('/delete/:id', function(req, res) {
         );
 });
 
-// Update Item
-
-// router.put('/update/:id', function(req, res){
-//     var data = req.params.id
-//     var name = req.body.item.name
-//     var category = req.body.item.category
-//     var price = req.body.item.price
-//     var quantity = req.body.item.quantity
-//     var description = req.body.item.description
-
-//     Items.update({
-//             name: name,
-//             category: category,
-//             price: price,
-//             quantity: quantity,
-//             description: description
-//         },
-//         {where: {id: data}} 
-//         ).then(
-//             function updateSuccess(updatedLog) {
-//                 res.json({
-//                     name: name,
-//                     category: category,
-//                     price: price,
-//                     quantity: quantity,
-//                     description: description
-//                 });
-//             }, 
-//             function updateError(err){
-//                 res.send(500, err.message);
-//             }
-//         )
-//     });
-
-    router.put('/:id', (req,res) => {
-        Item.update(req.body.item,{
-            where:{id: req.params.id, owner: req.user.id}
-        })
-            .then(item => res.status(200).json(item))
-            .catch(err => res.json({error: err}));
+//Update Item
+router.put('/:id', (req,res) => {
+    Item.update(req.body.item,{
+        where:{id: req.params.id, owner: req.user.id}
     })
+        .then(item => res.status(200).json(item))
+        .catch(err => res.json({error: err}));
+})
     
 //Get All Items
 router.get('/allitems', (req, res) => {
